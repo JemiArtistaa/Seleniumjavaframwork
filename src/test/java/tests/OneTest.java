4cd126e1235766b45d93fa06bcae0acf876ecee5
JemiArtistaa <mmislam020409@gmail.com>
2020-12-01 16:57:23 +0600

    end commit

--- src/test/java/tests/OneTest.java
+++ src/test/java/tests/OneTest.java
@@ -18,10 +18,10 @@ public class OneTest extends BaseClass{
 		driver=initializebrowser();
 		driver.get(prop.getProperty("url1"));
 		try {
-			Assert.assertEquals(driver.getTitle(),"Your Store");
+			Assert.assertEquals(driver.getTitle(),"Your Store is not yours");
 			System.out.println();
 		}catch(Exception e) {
-			System.out.println("Error");
+			System.out.println("Error is not yours");
 		}
 		
 	}
